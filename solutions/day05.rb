bcdec1a3f82071bbf7e540be341870d8e7fa10e2
rlchu <ryan.chupick@gmail.com>
2022-12-23 00:30:48 -0500

    day 05 in progress

--- solutions/day05.rb
+++ solutions/day05.rb
@@ -3,13 +3,42 @@
 module Day05
   class << self
 
-    def part1(pairs)
+    def part1(input)
+      crates = parse_crates(input)
+      moves = parse_moves(input)
+      binding.pry
     end
 
-    def part2(pairs)
+    def part2(input)
     end
 
     private
+
+    def move_crates(crates, iterations, from_index, to_index)
+      iterations.times do
+        ###
+
+      end
+    end
+
+    def parse_moves(input)
+      input.filter_map do |line|
+        line.scan(/\d+/).map(&:to_i)
+      end.reject{ _1.empty?}
+    end
+
+    def parse_crates(input)
+      crates = input
+        .take_while { _1[0..1] != " 1" }
+        .map { _1.chars.to_a}
+        .transpose
+
+      cc = []
+      crates.each_with_index do |val, index|
+        cc << val.filter{ _1 != " "}.reverse if index % 4 == 1
+      end
+      cc
+    end
   end
 end
 
