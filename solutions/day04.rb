f17f58d2d41c3a05f2b119ef2396c8b4afe24727
rlchu <ryan.chupick@gmail.com>
2022-12-22 23:11:28 -0500

    day04

--- solutions/day04.rb
+++ solutions/day04.rb
@@ -2,27 +2,29 @@
 
 module Day04
   class << self
-    PRIORITY_ORDER = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 
-    def part1(rucksacks)
-      rucksacks.map do
-        rucksack = _1.swapcase
-        priority = divide_into_compartments(rucksack.chars).inject(:&).sort.first.swapcase
-        PRIORITY_ORDER.find_index(priority)
-      end.sum
+    def part1(pairs)
+      scan_intersections(pairs) do |intersection,count,r1, r2|
+        intersection == r1 || intersection == r2 ? count+=1 : count
+      end
     end
 
-    def part2(rucksacks)
-      rucksacks.each_slice(3).map do |group|
-        priority = group.map { _1.swapcase.chars }.inject(:&).first.swapcase
-        PRIORITY_ORDER.find_index(priority)
-      end.sum
+    def part2(pairs)
+      scan_intersections(pairs) do |intersection, count, _, _|
+        !intersection.empty? ? count+=1 : count
+      end
     end
 
     private
 
-    def divide_into_compartments(rucksack)
-      rucksack.each_slice(rucksack.length/2)
+    def scan_intersections(pairs, &block)
+      pairs.reduce(0) do |count, pair|
+        p1,p2 =  pair.split(",").map{ _1.split("-") }
+        r1 = (p1.first.to_i..p1.last.to_i).to_a
+        r2 = (p2.first.to_i..p2.last.to_i).to_a
+        intersection = r1 & r2
+        yield intersection, count, r1, r2
+      end
     end
 
   end
--- spec/day04/day04_spec.rb
+++ spec/day04/day04_spec.rb
@@ -6,21 +6,21 @@ describe Day04 do
 
   context "part 1" do
     it "solves with sample data" do
-      expect(Day04.part1(sample_data)).to eq(157)
+      expect(Day04.part1(sample_data)).to eq(2)
     end
 
     it "solves with input" do
-      expect(Day04.part1(input)). to eq(7990)
+      expect(Day04.part1(input)). to eq(588)
     end
   end
 
   context "part 2" do
     it "solves with sample data" do
-      expect(Day04.part2(sample_data)).to eq(70)
+      expect(Day04.part2(sample_data)).to eq(4)
     end
 
     it "solves with input" do
-      expect(Day04.part2(input)). to eq(2602)
+      expect(Day04.part2(input)). to eq(911)
     end
   end
 end
