b4d07df851d23820e323eb8d81f1f83fc82217a0
rlchu <ryan.chupick@gmail.com>
2022-12-26 18:29:08 -0500

    ruby version bump and day5pt2

--- solutions/day05.rb
+++ solutions/day05.rb
@@ -1,30 +1,46 @@
-# frozen_string_literal: true
-
 module Day05
   class << self
 
     def part1(input)
       crates = parse_crates(input)
       moves = parse_moves(input)
-      binding.pry
+      moves.each do |move|
+        crates = move_crates(crates, move[0], move[1]-1, move[2]-1)
+      end
+      crates.map(&:last).join
     end
 
     def part2(input)
+      crates = parse_crates(input)
+      moves = parse_moves(input)
+      moves.each do |move|
+        crates = move_crates_in_batches(crates, move[0], move[1]-1, move[2]-1)
+      end
+      crates.map(&:last).join
     end
 
     private
 
+    def move_crates_in_batches(crates, quantity, from_index, to_index)
+      batch = []
+      quantity.times do
+        batch.push(crates[from_index].pop)
+      end
+      crates[to_index] = crates[to_index] + batch.reverse
+      crates
+    end
+
     def move_crates(crates, iterations, from_index, to_index)
       iterations.times do
-        ###
-
+        crates[to_index].push(crates[from_index].pop)
       end
+      crates
     end
 
     def parse_moves(input)
       input.filter_map do |line|
         line.scan(/\d+/).map(&:to_i)
-      end.reject{ _1.empty?}
+      end.reject{ _1.empty?}[1..]
     end
 
     def parse_crates(input)
