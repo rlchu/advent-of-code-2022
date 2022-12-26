8ed668b1485953e9106bf9db8c74067146857ca9
rlchu <ryan.chupick@gmail.com>
2022-12-26 18:45:59 -0500

    make day05 object oriented

--- solutions/day05.rb
+++ solutions/day05.rb
@@ -1,60 +1,57 @@
-module Day05
-  class << self
+class Day05
+  attr_accessor :crates, :moves
 
-    def part1(input)
-      crates = parse_crates(input)
-      moves = parse_moves(input)
-      moves.each do |move|
-        crates = move_crates(crates, move[0], move[1]-1, move[2]-1)
-      end
-      crates.map(&:last).join
+  def initialize(input)
+    @crates = parse_crates(input)
+    @moves = parse_moves(input)
+  end
+
+  def part1
+    moves.each do |move|
+      crates = move_crates(move[0], move[1]-1, move[2]-1)
     end
+    crates.map(&:last).join
+  end
 
-    def part2(input)
-      crates = parse_crates(input)
-      moves = parse_moves(input)
-      moves.each do |move|
-        crates = move_crates_in_batches(crates, move[0], move[1]-1, move[2]-1)
-      end
-      crates.map(&:last).join
+  def part2
+    moves.each do |move|
+      crates = move_crates_in_batches(move[0], move[1]-1, move[2]-1)
     end
+    crates.map(&:last).join
+  end
 
-    private
+  private
 
-    def move_crates_in_batches(crates, quantity, from_index, to_index)
-      batch = []
-      quantity.times do
-        batch.push(crates[from_index].pop)
-      end
-      crates[to_index] = crates[to_index] + batch.reverse
-      crates
+  def move_crates_in_batches(quantity, from_index, to_index)
+    batch = []
+    quantity.times do
+      batch.unshift(crates[from_index].pop)
     end
+    crates[to_index] = crates[to_index] + batch
+    crates
+  end
 
-    def move_crates(crates, iterations, from_index, to_index)
-      iterations.times do
-        crates[to_index].push(crates[from_index].pop)
-      end
-      crates
+  def move_crates(iterations, from_index, to_index)
+    iterations.times do
+      crates[to_index].push(crates[from_index].pop)
     end
+  end
 
-    def parse_moves(input)
-      input.filter_map do |line|
-        line.scan(/\d+/).map(&:to_i)
-      end.reject{ _1.empty?}[1..]
+  def parse_moves(input)
+    input.filter_map do |line|
+      line.scan(/\d+/).map(&:to_i)
+    end.reject{ _1.empty?}[1..]
+  end
+
+  def parse_crates(input)
+    cc = []
+    input.take_while { _1[0..1] != " 1" }
+      .map { _1.chars.to_a}
+      .transpose
+      .each_with_index do |val, index|
+      cc << val.filter{ _1 != " "}.reverse if index % 4 == 1
     end
-
-    def parse_crates(input)
-      crates = input
-        .take_while { _1[0..1] != " 1" }
-        .map { _1.chars.to_a}
-        .transpose
-
-      cc = []
-      crates.each_with_index do |val, index|
-        cc << val.filter{ _1 != " "}.reverse if index % 4 == 1
-      end
-      cc
-    end
+    cc
   end
 end
 
--- spec/day05/day05_spec.rb
+++ spec/day05/day05_spec.rb
@@ -6,21 +6,21 @@ describe Day05 do
 
   context "part 1" do
     it "solves with sample data" do
-      expect(Day05.part1(sample_data)).to eq("CMZ")
+      expect(Day05.new(sample_data).part1).to eq("CMZ")
     end
 
     it "solves with input" do
-      expect(Day05.part1(input)). to eq("SHQWSRBDL")
+      expect(Day05.new(input).part1).to eq("SHQWSRBDL")
     end
   end
 
   context "part 2" do
     it "solves with sample data" do
-      expect(Day05.part2(sample_data)).to eq("MCD")
+      expect(Day05.new(sample_data).part2).to eq("MCD")
     end
 
     it "solves with input" do
-      expect(Day05.part2(input)). to eq(nil)
+      expect(Day05.new(input).part2).to eq("CDTQZHBRS")
     end
   end
 end
