75cc1861fa9baa2127e703a2a445ffb0925c27db
rlchu <ryan.chupick@gmail.com>
2023-01-13 18:24:22 -0500

    day07 struggles

--- solutions/day07.rb
+++ solutions/day07.rb
@@ -11,18 +11,20 @@ class Day07
     @filestructure.each do |command|
       cd = command.scan(/\$ cd (.+)/).first&.first
       current_path << cd if cd
-      # current_path.uniq!
+      current_path.uniq!
       2.times { current_path.pop } if cd == ".."
       if command.to_i > 0
         current_path.each do |directory|
-          file_sizes[directory] += command.to_i
+          @file_sizes[directory] += command.to_i
         end
       end
     end
-    file_sizes.reduce(0) do |acc, val|
-      acc += val.last if val.last < 100_000
-      acc
+
+    total = 0
+    @file_sizes.each do |val|
+      total += val.last if val.last <= 100_000
     end
+    total
   end
 
   def part2
